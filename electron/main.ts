9af7e1a1a19d03676a6dc57f25798d342def1121
p0ria <p0ria@live.com>
2020-07-28 18:58:13 +0430

    typescript added

--- electron/main.ts
+++ electron/main.ts
@@ -1,48 +1,50 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var electron_1 = require("electron");
-var path = require('path');
-var url = require('url');
-var mainWindow;
-function createWindow() {
-    var startUrl = process.env.ELECTRON_START_URL || url.format({
+import { BrowserWindow, app } from "electron";
+const path = require('path');
+const url = require('url');
+
+let mainWindow: BrowserWindow | null;
+function createWindow () {
+    const startUrl = process.env.ELECTRON_START_URL || url.format({
         pathname: path.join(__dirname, '../index.html'),
         protocol: 'file:',
         slashes: true,
     });
-    mainWindow = new electron_1.BrowserWindow({
-        width: 800,
-        height: 600,
-        fullscreen: true,
-        autoHideMenuBar: true,
-        webPreferences: { nodeIntegration: true }
-    });
+    mainWindow = new BrowserWindow({
+        width: 800, 
+        height: 600, 
+        fullscreen: true, 
+        autoHideMenuBar: true, 
+        webPreferences: {nodeIntegration: true}});
     mainWindow.loadURL(startUrl);
     mainWindow.on('closed', function () {
         mainWindow = null;
     });
 }
-electron_1.app.on('ready', createWindow);
-electron_1.app.on('window-all-closed', function () {
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
-        electron_1.app.quit();
+        app.quit();
     }
 });
-electron_1.app.on('activate', function () {
+app.on('activate', function () {
     if (mainWindow === null) {
         createWindow();
     }
 });
+
 // var net = require('net');
+
 // console.log("creating connection...");
 // var client = new net.Socket();
 // client.connect(8001, '127.0.0.1', function() {
 //     console.log('Connected');
 //     client.write('Hello, server! Love, Client.');
 // });
+
 // client.on('data', function(data) {
 //     console.log('Received: ' + data);
 // });
+
 // client.on('close', function() {
 //     console.log('Connection closed');
 // });
